6bf28479825d5ba5a45557bb04acfb65f31416c0
aiden890 <khmin101@gmail.com>
2025-11-03 17:49:03 +0900

    add async script

--- src/lerobot/playground/script/async_client_smolvla.sh
+++ src/lerobot/playground/script/async_client_smolvla.sh
@@ -4,12 +4,12 @@ export PYTHONPATH=/Users/mingyeongho/Desktop/연구실/lerobot/src:$PYTHONPATH
 python -m lerobot.async_inference.robot_client \
   --robot.type=so101_follower \
   --robot.port=/dev/tty.usbmodem5AAF2641771 \
-  --robot.cameras="{ front: {type: opencv, index_or_path: 0, width: 640, height: 480, fps: 30}, top: {type: opencv, index_or_path: 1, width: 640, height: 480, fps: 30} }" \
+  --robot.cameras="{ camera1: {type: opencv, index_or_path: 0, width: 640, height: 480, fps: 30}, camera2: {type: opencv, index_or_path: 1, width: 640, height: 480, fps: 30}, camera3: {type: opencv, index_or_path: 1, width: 640, height: 480, fps: 30} }" \
   --robot.id=follower \
   --task="Grab the blue block and put in the cup" \
   --server_address=115.145.175.11:8080 \
-  --policy_type=pi0 \
-  --pretrained_name_or_path=vlasil/pi0_policy_pen_1101 \
+  --policy_type=smolvla \
+  --pretrained_name_or_path=cijerezg/smolvla-test \
   --policy_device=cuda \
   --actions_per_chunk=50 \
   --debug_visualize_queue_size=True 
